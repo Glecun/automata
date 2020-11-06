8543cf74baa35d759601699a2541737724b70066
Glecun <gregoire.lecun@gmail.com>
2020-11-06 20:18:03 +0100

    First commit

---
+++ Assets/Editor/GridScriptTest.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class GridScriptTest
+    {
+        [Test]
+        public void should_create_grid()
+        {
+            GridScript gridScript = new GridScript(1,2,3);
+            Assert.AreEqual(gridScript.GetCellSize(), 1);
+            Assert.AreEqual(gridScript.GetX(), 2);
+            Assert.AreEqual(gridScript.GetY(), 3);
+        }
+
+        [Test]
+        public void should_get_xy_with_world_position()
+        {
+            GridScript gridScript = new GridScript(10, 4, 4);
+            int x, y;
+            var worldPosition = new Vector3(32, 32);
+            gridScript.GetXY(worldPosition, out x, out y);
+
+            Assert.AreEqual(x, 3);
+            Assert.AreEqual(y, 3);
+           
+        }
+
+        [Test]
+        public void should_get_world_position_with_xy()
+        {
+            GridScript gridScript = new GridScript(10, 4, 4);
+            
+            var worldPosition = gridScript.GetWorldPosition(2,3);
+
+            var expectedWorldPosition = new Vector3(20, 30);
+
+            Assert.AreEqual(worldPosition, expectedWorldPosition);
+        }
+    }
+}
---
+++ Assets/Scripts/GridScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+public class GridScript {
+    private readonly int cellSize;
+    private readonly int x;
+    private readonly int y;
+
+    public GridScript(int cellSize, int x, int y)
+    {
+        this.cellSize = cellSize;
+        this.x = x;
+        this.y = y;
+    }
+
+    public int GetCellSize() { return cellSize; }
+    public int GetX() { return x; }
+    public int GetY() { return y; }
+
+    public Vector3 GetWorldPosition(int x, int y)
+    {
+        return new Vector3(x, y) * cellSize;
+    }
+
+    public void GetXY(Vector3 worldPosition, out int x, out int y)
+    {
+        x = Mathf.FloorToInt(worldPosition.x / cellSize);
+        y = Mathf.FloorToInt(worldPosition.y / cellSize);
+    }
+}
