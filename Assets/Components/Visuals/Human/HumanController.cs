9b3bac570b014c59de9198aa4d94f1d5973b9efa
Glecun <gregoire.lecun@gmail.com>
2020-11-07 22:04:42 +0100

    First implementation of human decision

--- Assets/Components/Visuals/Human/HumanController.cs
+++ Assets/Components/Visuals/Human/HumanController.cs
@@ -1,18 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class HumanController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private HumanDecisionController humanDecisionController;
 
-    // Update is called once per frame
-    void Update()
+    public HumanController()
     {
-        
+        humanDecisionController = new HumanDecisionController();
     }
 }
---
+++ Assets/Components/Visuals/Human/HumanDecisionController.cs
@@ -0,0 +1,14 @@
+﻿public enum Decision
+{
+    WAITING
+}
+
+public class HumanDecisionController
+{
+    public Decision currentDecision;
+
+    public HumanDecisionController()
+    {
+        currentDecision = Decision.WAITING;
+    }
+}
