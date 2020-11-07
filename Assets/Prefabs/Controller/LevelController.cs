1b2a20d7476d065eb5a6097265db6c491bd46543
Glecun <gregoire.lecun@gmail.com>
2020-11-07 18:11:19 +0100

    Add trees + correct background

--- Assets/Prefabs/Controller/LevelController.cs
+++ Assets/Prefabs/Controller/LevelController.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class LevelController : MonoBehaviour
 {
-    private void Awake()
+    private void Start()
     {
         var prefabList = GameObject.Find("ApplicationController").GetComponent<PrefabList>();
 
         InstantiateUtils.Instantiate(prefabList.get("BackgroundGrid"), new Vector3(0,0), Quaternion.identity, transform);
-        InstantiateUtils.Instantiate(prefabList.get("Tree"), new Vector3(0, 0), Quaternion.identity, transform);
+
+        //Trees
+        InstantiateUtils.Instantiate(prefabList.get("Tree"), new Vector3(20,3), Quaternion.identity, transform);
+        InstantiateUtils.Instantiate(prefabList.get("Tree"), new Vector3(20,4), Quaternion.identity, transform);
+        InstantiateUtils.Instantiate(prefabList.get("Tree"), new Vector3(20,5), Quaternion.identity, transform);
+        InstantiateUtils.Instantiate(prefabList.get("Tree"), new Vector3(19,4), Quaternion.identity, transform);
     }
 }
