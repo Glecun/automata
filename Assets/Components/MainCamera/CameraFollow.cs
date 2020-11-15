1e3f1662136789d4cb6fabf1bee0abca9944edd8
Glecun <gregoire.lecun@gmail.com>
2020-11-16 00:06:42 +0100

    WIP: Add Camera zoom + move behaviors

---
+++ Assets/Components/MainCamera/CameraFollow.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    private Camera myCamera;
+    private Func<Vector3> GetCameraFollowPositionFunc;
+    private Func<float> GetCameraZoomFunc;
+    private Vector2 xLimit;
+    private Vector2 yLimit;
+
+    private const float cameraZoomSpeed = 10f;
+    private const float cameraMoveSpeed = 2f;
+
+    public void Setup(Func<Vector3> GetCameraFollowPositionFunc, Func<float> GetCameraZoomFunc, Vector2 xLimit,
+        Vector2 yLimit)
+    {
+        this.GetCameraFollowPositionFunc = GetCameraFollowPositionFunc;
+        this.GetCameraZoomFunc = GetCameraZoomFunc;
+        this.xLimit = xLimit;
+        this.yLimit = yLimit;
+    }
+
+    private void Start()
+    {
+        myCamera = transform.GetComponent<Camera>();
+    }
+
+    public void SetCameraFollowPosition(Vector3 cameraFollowPosition)
+    {
+        SetGetCameraFollowPositionFunc(() => cameraFollowPosition);
+    }
+
+    public void SetGetCameraFollowPositionFunc(Func<Vector3> GetCameraFollowPositionFunc)
+    {
+        this.GetCameraFollowPositionFunc = GetCameraFollowPositionFunc;
+    }
+
+    public void SetCameraZoom(float cameraZoom)
+    {
+        SetGetCameraZoomFunc(() => cameraZoom);
+    }
+
+    public void SetGetCameraZoomFunc(Func<float> GetCameraZoomFunc)
+    {
+        this.GetCameraZoomFunc = GetCameraZoomFunc;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleMovement();
+        HandleZoom();
+
+        HandleLimit();
+    }
+
+    private void HandleLimit()
+    {
+        Vector3 transformPosition;
+        var position = transform.position;
+        transformPosition.x = Mathf.Clamp(position.x, xLimit.x, xLimit.y);
+        transformPosition.y = Mathf.Clamp(position.y, yLimit.x, yLimit.y);
+        transformPosition.z = position.z;
+        transform.position = transformPosition;
+    }
+
+    private void HandleMovement()
+    {
+        Vector3 cameraFollowPosition = GetCameraFollowPositionFunc();
+        cameraFollowPosition.z = transform.position.z;
+
+        Vector3 cameraMoveDir = (cameraFollowPosition - transform.position).normalized;
+        float distance = Vector3.Distance(cameraFollowPosition, transform.position);
+
+        if (distance > 0.1f)
+        {
+            Vector3 newCameraPosition =
+                transform.position + cameraMoveDir * distance * cameraMoveSpeed * Time.deltaTime;
+
+            float distanceAfterMoving = Vector3.Distance(newCameraPosition, cameraFollowPosition);
+
+            if (distanceAfterMoving > distance)
+            {
+                // Overshot the target
+                newCameraPosition = cameraFollowPosition;
+            }
+
+            transform.position = newCameraPosition;
+        }
+    }
+
+    private void HandleZoom()
+    {
+        float cameraZoom = GetCameraZoomFunc();
+
+        float cameraZoomDifference = cameraZoom - myCamera.orthographicSize;
+
+        myCamera.orthographicSize += cameraZoomDifference * cameraZoomSpeed * Time.deltaTime;
+
+        if (cameraZoomDifference > 0)
+        {
+            if (myCamera.orthographicSize > cameraZoom)
+            {
+                myCamera.orthographicSize = cameraZoom;
+            }
+        }
+        else
+        {
+            if (myCamera.orthographicSize < cameraZoom)
+            {
+                myCamera.orthographicSize = cameraZoom;
+            }
+        }
+    }
+}
--- Assets/Components/MainCamera/MainCameraController.cs
+++ Assets/Components/MainCamera/MainCameraController.cs
@@ -1,14 +1,68 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class MainCameraController : MonoBehaviour
 {
-    void Start()
+    [SerializeField] private CameraFollow cameraFollow;
+
+    private Vector3 cameraFollowPosition;
+    private float zoom = 10f;
+
+    private const float zoomChangeAmount = 150f;
+    private const float edgeSize = 50f;
+    private const float moveAmount = 30f;
+    private const float zoomMin = 5f;
+    private const float zoomMax = 10f;
+
+    private void Awake()
     {
-        float aspectRatio = Camera.main.aspect; //(width divided by height)
-        float camSize = Camera.main.orthographicSize; //The size value mentioned earlier
-        float correctPositionX = aspectRatio * camSize;
-        Camera.main.transform.position = new Vector3(correctPositionX, camSize, -10);
+        cameraFollow.Setup(() => cameraFollowPosition, () => zoom, new Vector2(0, 22), new Vector2(0, 14));
+    }
+
+    private void Update()
+    {
+        HandleEdgeMovement();
+        HandleZoom();
+    }
+
+    private void HandleZoom()
+    {
+        if (Input.mouseScrollDelta.y > 0)
+        {
+            zoom -= zoomChangeAmount * Time.deltaTime;
+        }
+
+        if (Input.mouseScrollDelta.y < 0)
+        {
+            zoom += zoomChangeAmount * Time.deltaTime;
+        }
+
+        zoom = Mathf.Clamp(zoom, zoomMin, zoomMax);
+    }
+
+    private void HandleEdgeMovement()
+    {
+        if (Input.mousePosition.x > Screen.width - edgeSize)
+        {
+            //EDGE RIGHT
+            cameraFollowPosition.x += moveAmount * Time.deltaTime;
+        }
+
+        if (Input.mousePosition.x < edgeSize)
+        {
+            //EDGE LEFT
+            cameraFollowPosition.x -= moveAmount * Time.deltaTime;
+        }
+
+        if (Input.mousePosition.y > Screen.height - edgeSize)
+        {
+            //EDGE UP
+            cameraFollowPosition.y += moveAmount * Time.deltaTime;
+        }
+
+        if (Input.mousePosition.y < edgeSize)
+        {
+            //EDGE DOWN
+            cameraFollowPosition.y -= moveAmount * Time.deltaTime;
+        }
     }
 }
