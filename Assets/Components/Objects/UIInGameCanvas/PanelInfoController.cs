c11744209462babb1c81a49ea6797c4fd4643338
Glecun <gregoire.lecun@gmail.com>
2020-12-01 00:32:04 +0100

    Add TownHall Food Amount on UI

--- Assets/Components/Objects/UIInGameCanvas/PanelInfoController.cs
+++ Assets/Components/Objects/UIInGameCanvas/PanelInfoController.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 public class PanelInfoController : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI woodAmount = null;
+    [SerializeField] private TextMeshProUGUI foodAmount = null;
     private TownHall townHall;
 
     private void Update()
     {
         townHall = Utils.initWhenFound(townHall, () => GameObject.Find("TownHall").GetComponent<TownHall>());
         woodAmount.text = townHall != null ? townHall.getResource(ResourceEnum.WOOD).amount.ToString() : "0";
+        foodAmount.text = townHall != null ? townHall.getResource(ResourceEnum.FOOD).amount.ToString() : "0";
     }
 }
